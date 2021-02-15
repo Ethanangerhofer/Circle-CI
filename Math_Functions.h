f93060882da519263e217f353b766648744bb938
Ethan <ethan@localhost.localdomain>
2021-02-15 14:00:10 -0500

    test

---
+++ Math_Functions.h
@@ -0,0 +1,21 @@
+//Header File with some random Test Functions
+
+float PeriodicFunction(float x, float Period)
+{
+	float out = sin(2 * M_PI * x / Period);
+	return out;
+}
+
+float Abs(float input)
+{
+	if (input < 0)
+		input = -input;
+		
+	return input;
+}
+
+float Inverse(float input)
+{
+	float out = 1/input;
+	return out;
+}
